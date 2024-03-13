347dcd64019dd56ff390c17fe0e9d9d9d5a4e9f9
Kevin Ju <kevjuliow@gmail.com>
2024-03-13 13:26:15 +0700

    godotenv

--- app/database.go
+++ app/database.go
@@ -3,12 +3,22 @@ package app
 import (
 	"database/sql"
 	_ "github.com/go-sql-driver/mysql"
+	"github.com/joho/godotenv"
 	"golang-restful-api/helper"
+	"os"
 	"time"
 )
 
+func LoadEnv() {
+	err := godotenv.Load()
+	helper.PanicIfError(err)
+}
 func NewDb() *sql.DB {
-	db, err := sql.Open("mysql", "root:ewk241xv28@tcp(localhost:3306)/golang_restful_api")
+	LoadEnv()
+	dbName := os.Getenv("DB_USERNAME")
+	dbpassword := os.Getenv("DB_PASSWORD")
+	dbUrl := dbName + ":" + dbpassword + "@tcp(localhost:3306)/golang_restful_api"
+	db, err := sql.Open("mysql", dbUrl)
 	helper.PanicIfError(err)
 
 	db.SetMaxOpenConns(20)
--- go.mod
+++ go.mod
@@ -7,6 +7,7 @@ require (
 	github.com/go-playground/universal-translator v0.18.1 // indirect
 	github.com/go-playground/validator v9.31.0+incompatible // indirect
 	github.com/go-sql-driver/mysql v1.7.1 // indirect
+	github.com/joho/godotenv v1.5.1 // indirect
 	github.com/julienschmidt/httprouter v1.3.0 // indirect
 	github.com/leodido/go-urn v1.4.0 // indirect
 )
